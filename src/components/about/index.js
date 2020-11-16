112b89eee5cebee59caadc34020a0cba123e8685
liquidsword <audenrobertson@gmail.com>
2020-11-16 16:17:17 -0500

    updating about page

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -232,12 +232,17 @@ class About extends React.Component{
     render(){
         return(
             <section className='about'>
-                <h1 className='about__main-title'>In the City of Wind...</h1>
+
+                <div className='about__main-title'>
+                    <h2>In the City of Wind...</h2>
+                </div>
 
                 <div className='about__intro'>
-                    <h3 className='about__intro-title'>
-                        What we believe
-                    </h3>
+                    <div className='about__intro-title'>
+                        <h4>What we<br></br> 
+                        believe
+                        </h4>
+                    </div>
                     <div className='about__intro-info'>
                         <p>
                             viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis
